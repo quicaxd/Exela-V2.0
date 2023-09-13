16ce055dc5d995011a2bc8cca8b5e5e2622c8e78
quicaxd <127431653+quicaxd@users.noreply.github.com>
2023-09-13 20:12:31 +0300

    Update obf.py

--- Obfuscator/obf.py
+++ Obfuscator/obf.py
@@ -1,14 +1,9 @@
-import os
 import argparse
 from cryptography.fernet import Fernet
 import base64
 import random
 import marshal
 import zlib
-from colorama import Fore, init
-init()
-cmd = 'mode 115,25'
-os.system(cmd)
 
 
 parser = argparse.ArgumentParser(description="Python Code Obfuscator")
@@ -107,10 +102,8 @@ stub2 = f"""__VareObfuscator__ = ''
 import base64 as ______;import marshal as ____;import zlib as __________;from cryptography.fernet import Fernet;import base64;__mikey__="{base64.b64encode(stubkey).decode()}";mydata="{hex_str}";__vare__ = lambda x: ____.loads(__________.decompress(______.b32decode(______.b64decode(x[::-1]))));__mycip__= Fernet(base64.b64decode(__mikey__));__step1__=bytes.fromhex(mydata);__step2__=__mycip__.decrypt(__step1__);__decr__=base64.b64decode(__step2__);__decrdata__=__decr__;__gotnew__=base64.b32decode(__decr__);__newdecr__={random.randint(999999,999999999999)};__getnew__=__newdecr__;__myb64code__=base64.b64decode(__gotnew__);__myb64codee__=base64.b64decode(__myb64code__);___ = __myb64codee__;exec(__vare__(___))"""
 
 
-filename = 'Obfuscated.py'
-with open('Obfuscated.py', "w") as file:
+with open('Stub.py', "w") as file:
     file.write(stub)
 
-os.system('clear||cls')
-print(f'{Fore.LIGHTCYAN_EX}[{Fore.RESET}{Fore.CYAN}+{Fore.RESET}{Fore.LIGHTCYAN_EX}]{Fore.RESET} File obfuscated succesffuly\n')
+print(f'File obfuscated succesffuly\n')
 
