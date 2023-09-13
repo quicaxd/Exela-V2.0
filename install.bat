ca1a895aa66e7886e7fa364d4f8a2b7583b02c8f
quicaxd <127431653+quicaxd@users.noreply.github.com>
2023-09-13 20:08:07 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,11 @@
+@echo off
+cd /d %~dp0
+
+title Checking Python installation...
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the python 3.11.0 or 3.10.0^)
+    echo Very important click Add python exe to PATH.
+    pause
+)
 python builder.py
