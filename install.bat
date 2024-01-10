2b4042fcd3194b2fce84176653749a8b3767ded8
quicaxd <127431653+quicaxd@users.noreply.github.com>
2024-01-10 21:43:09 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,8 +4,32 @@ cd /d %~dp0
 title Checking Python installation...
 python --version > nul 2>&1
 if %errorlevel% neq 0 (
-    echo Python is not installed! (Go to https://www.python.org/downloads and install the python 3.11.0 or 3.10.0^)
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the python 3.11.0^)
     echo Very important click Add python exe to PATH.
     pause
 )
+
+title Checking Python libraries...
+echo Checking 'cryptography' (1/3)
+python -c "import cryptography" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing cryptography...
+    python -m pip install cryptography > nul
+)
+
+echo Checking 'aiohttp' (2/3)
+python -c "import aiohttp" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing aiohttp...
+    python -m pip install aiohttp > nul
+)
+
+echo Checking 'pyinstaller' (3/3)
+python -c "import PyInstaller" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pyinstaller...
+    python -m pip install pyinstaller > nul
+)
+
+cls
 python builder.py
